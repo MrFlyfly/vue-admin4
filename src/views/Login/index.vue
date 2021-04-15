6f229a94ff6ea0a751770882d41f8697863b02d8
HuangJF <13188128537@139.com>
2021-04-15 14:30:57 +0800

    构建项目，导入element ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        哈哈
+        <el-button type="primary">主要按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
